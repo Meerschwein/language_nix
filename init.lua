3bbcb60ed8a06296060e5d06ccfbe4afc7957939
Merlin Volkmer <merlin.volkmer@outlook.com>
2023-01-01 17:23:25 +0100

    add string interpolation

--- init.lua
+++ init.lua
@@ -2,86 +2,101 @@
 -- https://nixos.wiki/wiki/Overview_of_the_Nix_Language
 local syntax = require "core.syntax"
 
+local function combine_patterns(t1, t2)
+  local temp = { table.unpack(t1) }
+  for _, t in ipairs(t2) do
+    table.insert(temp, t)
+  end
+  return temp
+end
+
+local function merge_tables(a, b)
+  for _, v in pairs(b) do
+    table.insert(a, v)
+  end
+end
+
+local normalSymbols = {
+  ["import"]   = "keyword2",
+  ["with"]     = "keyword2",
+  ["builtins"] = "keyword2",
+  ["inherit"]  = "keyword2",
+  ["assert"]   = "keyword2",
+  ["let"]      = "keyword2",
+  ["in"]       = "keyword2",
+  ["rec"]      = "keyword2",
+  ["if"]       = "keyword",
+  ["else"]     = "keyword",
+  ["then"]     = "keyword",
+  ["true"]     = "literal",
+  ["false"]    = "literal",
+  ["null"]     = "literal",
+}
+
+local default_patterns = {}
+local doubleQuoteString = {}
+local multilineString = {}
+local stringInterpolation = {}
+
+
+merge_tables(stringInterpolation, {
+  { pattern = {"%${", "}"}, type = "keyword2", syntax = {
+    patterns = default_patterns,
+    symbols = normalSymbols,
+  }},
+  { pattern = '[%S][%w]*', type = "string" },
+})
+
+merge_tables(doubleQuoteString, {
+  { pattern = { '"', '"'}, type = "string", syntax = {
+    patterns = stringInterpolation,
+    symbols = {},
+  }},
+})
+
+merge_tables(multilineString, {
+  { pattern = {"''", "''"}, type = "string", syntax = {
+    patterns = stringInterpolation,
+    symbols = {},
+  }},
+})
+
+merge_tables(default_patterns,
+  combine_patterns(
+    combine_patterns(doubleQuoteString, multilineString), {
+  { pattern = "#.-\n",            type = "comment" },
+  { pattern = { "/%*", "%*/" },   type = "comment" },
+  { pattern = "-?%.?%d+",         type = "number"  },
+  -- interpolation
+  { pattern = {"%${", "}"}, type = "keyword2", syntax = {
+    patterns = default_patterns
+    ,
+    symbols = normalSymbols,
+  }},
+  -- operators
+  { pattern = "[%+%-%?!<>%*/]", type = "operator" },
+  { pattern = "//",             type = "operator" },
+  { pattern = "&&",             type = "operator" },
+  { pattern = "%->",            type = "operator" },
+  { pattern = "||",             type = "operator" },
+  { pattern = "==",             type = "operator" },
+  { pattern = "!=",             type = "operator" },
+  { pattern = ">=",             type = "operator" },
+  { pattern = "<=",             type = "operator" },
+  -- paths
+  { pattern = "%.?%.?/[%w_%-/%.]+", type = "string" },
+  { pattern = { "<", ">" },         type = "string" },
+  -- every other symbol
+  { pattern = "[%a%-%_][%w%-%_]*", type = "symbol" },
+  { pattern = ";%.,:",             type = "normal" },
+  { pattern = "%.%.%.",            type = "normal" },
+}))
+
 syntax.add {
   name = "Nix",
   files = { "%.nix$" },
   comment = "#",
   block_comment = { "/*", "*/" },
-  patterns = {
-    { pattern = "#.-\n",            type = "comment" },
-    { pattern = { "/%*", "%*/" },   type = "comment" },
-    { pattern = { "''", "''" },     type = "string"  },
-    { pattern = { '"', '"', "\\" }, type = "string"  },
-    { pattern = "-?%.?%d+",         type = "number"  },
-    -- interpolation
-    { pattern = "%${().-()}", type = { "keyword2", "symbol", "keyword2" } },
-    -- operators
-    { pattern = "[%+%-%?!<>%*/]", type = "operator" },
-    { pattern = "//",             type = "operator" },
-    { pattern = "&&",             type = "operator" },
-    { pattern = "%->",            type = "operator" },
-    { pattern = "||",             type = "operator" },
-    { pattern = "==",             type = "operator" },
-    { pattern = "!=",             type = "operator" },
-    { pattern = ">=",             type = "operator" },
-    { pattern = "<=",             type = "operator" },
-    -- paths
-    { pattern = "%.?%.?/[%w_%-/%.]+", type = "string" },
-    { pattern = { "<", ">" },         type = "string" },
-    -- every other symbol
-    { pattern = "[%a%-%_][%w%-%_]*", type = "symbol" },
-    { pattern = ";%.,:",             type = "normal" },
-    { pattern = "%.%.%.",            type = "normal" },
-  },
-  symbols = {
-    ["import"]   = "keyword2",
-    ["with"]     = "keyword2",
-    ["builtins"] = "keyword2",
-    ["inherit"]  = "keyword2",
-    ["assert"]   = "keyword2",
-    ["let"]      = "keyword2",
-    ["in"]       = "keyword2",
-    ["rec"]      = "keyword2",
-    ["if"]       = "keyword",
-    ["else"]     = "keyword",
-    ["then"]     = "keyword",
-    ["true"]     = "literal",
-    ["false"]    = "literal",
-    ["null"]     = "literal",
-  }
+  patterns = default_patterns,
+  symbols = normalSymbols,
 }
-
--- This is the start of proper string interpolation but I gave up
--- local function combine_patterns(t1, t2)
---   local temp = { table.unpack(t1) }
---   for _, t in ipairs(t2) do
---     table.insert(temp, t)
---   end
---   return temp
--- end
---
--- local function merge_tables(a, b)
---   for _, v in pairs(b) do
---     table.insert(a, v)
---   end
--- end
---
--- local doubleQuoteString = {}
---
--- local curlys = combine_patterns({
---   { pattern = {"%${", "}"},  type = "keyword2", syntax = {
---     patterns = doubleQuoteString,
---     symbols = {},
---   }},
---   { pattern = '[%a][%w]*', type = "string" },
--- }, normal)
---
--- merge_tables(doubleQuoteString, combine_patterns({
---   { pattern = { '"', '"', "\\"}, type = "string", syntax = {
---     patterns = curlys,
---     symbols = {},
---   }},
---   { pattern = "[%a][%w]*", type = "literal" },
---   }, normal)
--- )
-
